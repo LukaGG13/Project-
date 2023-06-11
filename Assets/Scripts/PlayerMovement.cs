d7d854fbc4653ebb757d98feeb69952d76707d6e
Cactus-John <ivan.ivo.bosnjak@gmail.com>
2023-06-11 18:47:32 +0200

    weapon asset, terrain

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -47,7 +47,7 @@ public class PlayerMovement : MonoBehaviour
     {
         // ground check
 
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, whatIsGround);
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
 
         MyInput();
         SpeedControl();
